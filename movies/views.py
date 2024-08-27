b675bf41dd7146451aa722c4dd2b4d54abab89fe
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2024-08-27 17:46:54 -0300

    Create Serializers

--- movies/views.py
+++ movies/views.py
@@ -1,1 +1,12 @@
+from rest_framework import generics
+from movies.models import Movie
+from movies.serializers import MovieSerializer
 
+
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieRetriverUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
