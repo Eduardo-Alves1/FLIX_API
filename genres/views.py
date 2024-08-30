467ffa1b731b743f11fda657bbc3ce76bebd78d1
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2024-08-30 10:29:49 -0300

    Update Urls, TokenVerify and TokenRefresh

--- genres/views.py
+++ genres/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from genres.models import Genres
 from genres.serializers import GenreSerializer
  
@@ -6,11 +7,13 @@ from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genres.objects.all()
     serializer_class = GenreSerializer
 
 
 
 class GenreRetrieverUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genres.objects.all()
     serializer_class = GenreSerializer
--- movies/serializers.py
+++ movies/serializers.py
@@ -1,11 +1,23 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        return None
+
+
     
     def validate_release_date(self, value):
         if value.year < 1990:
